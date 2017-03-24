059901540826664fc0ed9402b368fe8d964771cc
Raphael Guntersweiler <raphael@rgunti.ch>
2017-03-24 01:07:46 +0100

    Implemented User List via AJAX (a bunch of improvements had to be made in order for this to work properly)

--- index.php
+++ index.php
@@ -7,12 +7,14 @@ $request = RequestProcessor::getProcessedRequest();
 
 $themes = [
     'default' => '/css/bootstrap.min.css',
+    'defaulttheme' => '/css/bootstrap-theme.min.css',
     'darkmode' => '/css/bootswatch.min.css',
     'darkadmin' => '/css/darkadmin.min.css',
     'superhero' => '/css/superhero.min.css',
 ];
 $usedTheme = $themes[AppUtils::getDefaultValue(@$_COOKIE['UsedTheme'], 'darkmode')];
 ?>
+<!DOCTYPE html>
 <html>
     <head>
         <title>
@@ -24,32 +26,43 @@ $usedTheme = $themes[AppUtils::getDefaultValue(@$_COOKIE['UsedTheme'], 'darkmode
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <meta name="HandheldFriendly" content="true" />
 
+        <!-- STYLESHEETS -->
+        <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700">
+
+        <!-- Bootstrap -->
         <link rel="stylesheet" href="<?= PathUtils::getLink('/css/bootstrap.min.css') ?>" />
+
+        <?php if ($usedTheme != $themes['default']) { ?>
+        <!-- Bootstrap Theme -->
         <link rel="stylesheet" href="<?= PathUtils::getLink($usedTheme) ?>" />
+        <?php } ?>
+
+        <!-- jQuery DataTables -->
+        <link rel="stylesheet" href="<?= PathUtils::getLink('/css/dataTables.bootstrap.min.css') ?>" />
         <!--
-        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.13/css/dataTables.bootstrap.min.css" />
-        <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.1.1/css/responsive.dataTables.min.css" />
-        -->
+        <link rel="stylesheet" href="<?= PathUtils::getLink('/css/responsive.dataTables.min.css') ?>" />
         <link rel="stylesheet" href="<?= PathUtils::getLink('/css/bootstrap-checkbox/awesome-bootstrap-checkbox.css') ?>" />
+        -->
         <link rel="stylesheet" href="<?= PathUtils::getLink('/css/rgunti.css') ?>" />
 
+        <!-- JAVASCRIPT -->
         <script src="<?= PathUtils::getLink('/js/jquery.min.js') ?>"></script>
         <script src="<?= PathUtils::getLink('/js/tether.min.js') ?>"></script>
         <script src="<?= PathUtils::getLink('/js/bootstrap.min.js') ?>"></script>
+        <script src="<?= PathUtils::getLink('/js/parallax.min.js') ?>"></script>
+
+        <!-- jQuery DataTables (+ Addons) -->
+        <script src="<?= PathUtils::getLink('/js/jquery.dataTables.min.js') ?>"></script>
+        <script src="<?= PathUtils::getLink('/js/dataTables.bootstrap.min.js') ?>"></script>
+        <script src="<?= PathUtils::getLink('/js/dataTables.fixedColumns.min.js') ?>"></script>
         <!--
-        <script src="js/parallax.min.js"></script>
+        <script src="<?= PathUtils::getLink('/js/dataTables.responsive.min.js') ?>"></script>
         -->
 
-        <!--
-        <script src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdn.datatables.net/1.10.13/js/dataTables.bootstrap.min.js"></script>
-        <script src="https://cdn.datatables.net/fixedcolumns/3.2.2/js/dataTables.fixedColumns.min.js"></script>
-        <script src="https://cdn.datatables.net/responsive/2.1.1/js/dataTables.responsive.min.js"></script>
-        -->
-
-        <script src="js/rgunti.js"></script>
+        <!-- Custom Scripts -->
+        <script src="<?= PathUtils::getLink('/js/rgunti.js') ?>"></script>
     </head>
     <body>
         <nav class="navbar navbar-default navbar-fixed-top">
--- js/rgunti.js
+++ js/rgunti.js
@@ -1,3 +1,12 @@
 /**
- * Created by rapha on 16.03.2017.
+ * Returns a glyphicon span with a checkmark if the given parameter is true
+ * @param is_true bool
+ * @returns {*}
  */
+function getHtmlCheckbox(is_true) {
+    if (is_true) {
+        return '<span class="glyphicon glyphicon-ok"></span>';
+    } else {
+        return '';
+    }
+}
