e5600a48b1b9bb7470f50d57c991be24de75fb5b
Raphael Guntersweiler <raphael@rgunti.ch>
2017-03-25 02:36:25 +0100

    Enabling and Disabling Users is now implemented (using Modals and AJAX)

---
+++ js/ajaxSubmitTools.js
@@ -0,0 +1,44 @@
+
+function onAjaxError(jqXHR, textStatus, errorThrown) {
+    console.log(this, jqXHR, textStatus, errorThrown);
+
+    var errorText = '';
+    if (jqXHR.status == 404) {
+        errorText += 'Error 404: The requested resource could not be found.\n';
+        errorText += 'Resource Requested: ' + this.url;
+    } else {
+        errorText += 'HTTP Error:         ' + jqXHR.status + ' ' + errorThrown + '\n';
+        errorText += 'Resource Requested: ' + this.url + '\n';
+        errorText += 'Method Requested:   ' + this.method + '\n';
+        errorText += 'Data Transmitted:   ' + this.data + '\n';
+        errorText += '\n';
+        if (jqXHR.responseJSON) {
+            errorText += 'Sever provided information:\n';
+            if (typeof jqXHR.responseJSON.ErrorInfo == 'string') {
+                errorText += jqXHR.responseJSON.ErrorInfo;
+            } else {
+                errorText += JSON.stringify(jqXHR.responseJSON.ErrorInfo, '   ');
+            }
+        } else {
+            errorText += 'No additional information is available.\nThis is the first part of the response:\n\n';
+            errorText += 'Total Size of response: ~ ' + jqXHR.responseText.length + ' bytes';
+            errorText += '\n --- START OF RESPONSE --- \n';
+            errorText += escapeHtml(limitString(jqXHR.responseText, 512, '\n -[ trimmed after 512 bytes ]-'));
+            errorText += '\n --- END OF RESPONSE --- ';
+        }
+    }
+
+    showUnhandledError(errorText);
+}
+
+function submitSimplePostRequest(_url,_data,onBeforeSend,onSuccess,onError,onComplete) {
+    $.ajax({
+        url: _url,
+        method: 'post',
+        data: _data,
+        beforeSend: onBeforeSend,
+        success: onSuccess,
+        error: (onError == null) ? onAjaxError : onError,
+        complete: onComplete
+    });
+}
--- js/rgunti.js
+++ js/rgunti.js
@@ -10,3 +10,38 @@ function getHtmlCheckbox(is_true) {
         return '';
     }
 }
+
+/**
+ * Source:
+ * @param text string
+ * @returns {XML|void|string}
+ */
+function escapeHtml(text) {
+    var map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#039;'
+    };
+
+    return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+}
+
+/**
+ * Displays the Unhandled Error Modal Box along with some given information about the error reported
+ * @param errorText string
+ */
+function showUnhandledError(errorText) {
+    $('#unhandledErrorInfo').html(errorText);
+    $('#unhandledErrorDialog').modal('show');
+}
+
+
+function limitString(s,max,trim) {
+    if (s.length > max) {
+        return s.substring(0, max) + trim;
+    } else {
+        return s;
+    }
+}
